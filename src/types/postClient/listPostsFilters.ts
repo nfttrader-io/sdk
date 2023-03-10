46a34eba1e4c335ceebb33d85492f431ee2b426f
Mattia <matticrypto@gmail.com>
2023-03-10 16:19:47 +0100

    wip

--- src/types/postClient/listPostsFilters.ts
+++ src/types/postClient/listPostsFilters.ts
@@ -50,8 +50,4 @@ export default interface ListPostsFilters {
    * Filter posts that are going to expire before or at `expirationDate` timestamp
    */
   expirationDate?: number
-  /**
-   * Filter posts including also the parent of each post
-   */
-  includeParent?: boolean
 }
