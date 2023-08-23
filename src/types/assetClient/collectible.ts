bff5a76f87730e6369fda88294508c103ae7ea92
Mattia <matticrypto@gmail.com>
2023-08-23 11:37:54 +0100

    chore -> Collectible type updated

--- src/types/assetClient/collectible.ts
+++ src/types/assetClient/collectible.ts
@@ -28,6 +28,56 @@ export interface Collectible {
         traitType: Maybe<string>
         value: Maybe<string>
       }>
+    | {
+        contract: {
+          address: Maybe<string>
+          name: Maybe<string>
+          symbol: Maybe<string>
+          totalSupply: Maybe<number>
+          tokenType: Maybe<string>
+          contractDeployer: Maybe<string>
+          deployedBlockNumber: Maybe<number>
+          openSeaMetadata: {
+            floorPrice: Maybe<number> | Maybe<string>
+            collectionName: Maybe<string>
+            collectionSlug: Maybe<string>
+            safelistRequestStatus: Maybe<string>
+            imageUrl: Maybe<string>
+            description: Maybe<string>
+            externalUrl: Maybe<string>
+            twitterUsername: Maybe<string>
+            discordUrl: Maybe<string>
+            bannerImageUrl: Maybe<string>
+            lastIngestedAt: Maybe<string>
+          }
+          isSpam: boolean
+          spamClassifications: Array<string>
+        }
+        tokenId: string
+        tokenType: string
+        name: Maybe<string>
+        description: Maybe<string>
+        tokenUri: Maybe<string>
+        image: {
+          cachedUrl: Maybe<string>
+          thumbnailUrl: Maybe<string>
+          pngUrl: Maybe<string>
+          contentType: Maybe<string>
+          size: Maybe<string>
+          originalUrl: Maybe<string>
+        }
+        raw: {
+          tokenUri: Maybe<string>
+          metadata: any
+          error: any
+        }
+        timeLastUpdated: Maybe<string>
+        balance: Maybe<string>
+        acquiredAt: {
+          blockTimestamp: Maybe<string> | Maybe<number>
+          blockNumber: Maybe<string> | Maybe<number>
+        }
+      }
   metadataUrl: Maybe<string>
   owner: string | string[]
   isOwner?: boolean
