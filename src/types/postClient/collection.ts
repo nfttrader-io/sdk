1fc2889c0fcc42e113e1c5acbad1c66add2f108f
Mattia <matticrypto@gmail.com>
2023-08-23 13:34:13 +0100

    chore -> Collection interface updated

--- src/types/postClient/collection.ts
+++ src/types/postClient/collection.ts
@@ -4,11 +4,11 @@ export default interface Collection {
   name: string
   statusVerification: -1 | 0 | 1 | 2
   address: string
-  imageUrl: string
+  imageUrl: Maybe<string>
   networkId: string
-  abi: Array<any>
+  abi: Maybe<Array<any>>
   type: "ERC721" | "ERC1155" | "ERC20" | "NATIVE"
-  symbol: string
+  symbol: Maybe<string>
   createdAt: Maybe<string>
   isFavourite?: 0 | 1
   notification?: boolean
