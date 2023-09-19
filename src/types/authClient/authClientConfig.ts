e70aefe5e4230d2034802bb03e83bf711395b65c
Mattia <matticrypto@gmail.com>
2023-09-19 12:28:41 +0200

    chore -> fix AuthClientConfig

--- src/types/authClient/authClientConfig.ts
+++ src/types/authClient/authClientConfig.ts
@@ -2,9 +2,9 @@ import Maybe from "../general/maybe"
 import AuthMode from "./authMode"
 
 export type AuthClientConfig = {
-  backendURL: Maybe<string>
-  mode: Maybe<AuthMode>
-  serviceName: Maybe<string>
-  serviceTOSURL: Maybe<string>
-  servicePrivacyURL: Maybe<string>
+  backendURL?: Maybe<string>
+  mode?: Maybe<AuthMode>
+  serviceName?: Maybe<string>
+  serviceTOSURL?: Maybe<string>
+  servicePrivacyURL?: Maybe<string>
 }
