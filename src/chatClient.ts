5cc75b32b3062c0f5513adb6d2edf94a187be3dd
Mattia Migliore <mattiamigliore.com@gmail.com>
2024-03-24 12:12:38 +0100

    wip

---
+++ src/chatClient.ts
@@ -0,0 +1,35 @@
+import GlobalFetch from "./lib/globalFetch"
+import { ChatConfig } from "./types/chatClient/chatConfig"
+import { Client, cacheExchange, fetchExchange } from "@urql/core"
+
+export default class ChatClient extends GlobalFetch {
+  private _jwtToken: string | null = null
+  private _apiKey: string | null = null
+  private _graphQLEndpoint: string | null = null
+  private _graphQLRealtimeEndpoint: string | null = null
+  private _realTimeAuthorizationToken: string | null = null
+  private _client: Client | null = null
+
+  constructor(config: ChatConfig) {
+    super()
+    this._jwtToken = config.jwtToken
+    this._apiKey = config.apiKey
+    this._graphQLEndpoint = config.graphQLEndpoint
+    this._graphQLRealtimeEndpoint = config.graphQLRealtimeEndpoint
+    this._realTimeAuthorizationToken = `${this._apiKey}##${this._jwtToken}`
+
+    try {
+      this._client = new Client({
+        url: this._graphQLEndpoint,
+        fetchOptions: () => {
+          return {
+            headers: { Authorization: `${this._apiKey}` },
+          }
+        },
+        exchanges: [cacheExchange, fetchExchange],
+      })
+    } catch (err) {
+      console.error(err)
+    }
+  }
+}
---
+++ src/types/chatClient/chatConfig.ts
@@ -0,0 +1,6 @@
+export type ChatConfig = {
+  jwtToken: string
+  apiKey: string
+  graphQLEndpoint: string
+  graphQLRealtimeEndpoint: string
+}
